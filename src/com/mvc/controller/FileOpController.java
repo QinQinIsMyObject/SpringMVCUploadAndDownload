f89d387a6d0a26cff2e7751f09c8477f70af3846
XieFucai <1977596495@qq.com>
2020-04-08 17:48:26 +0800

    完善了上传代码

--- src/com/mvc/controller/FileOpController.java
+++ src/com/mvc/controller/FileOpController.java
@@ -3,8 +3,16 @@
  */
 package com.mvc.controller;
 
+import java.io.File;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Shieh
@@ -20,10 +28,26 @@ public class FileOpController {
 		return "index";
 	}
 
-//	@RequestMapping("/doUpload.do")
-//	// 文件上传
-//	// 注意：参数里面要有MultipartFile这个参数且file名称要和表单的name属性值保持一致
-//	public ModelAndView fileUploadMethod(MultipartFile file, HttpServletRequest request) {
-//		return null;
-//	}
+	@RequestMapping("/doUpload.do")
+	// 文件上传
+	// 注意：参数里面要有MultipartFile这个参数且file名称要和表单的name属性值保持一致
+	public ModelAndView fileUploadMethod(MultipartFile file, HttpServletRequest request) throws Exception {
+		// 1、获得文件名称；
+		String fname = file.getOriginalFilename();
+		// 2、对文件进行处理(例如生成随机文件名)
+		// a.获取文件的后缀名
+		String txt = FilenameUtils.getExtension(fname);
+		// b.使用UUID生成随机文件名--推荐，可以接近全球唯一
+//		String docmString=System.currentTimeMillis()+"."+docm;
+		String docm = UUID.randomUUID() + "." + txt;
+		System.out.println(docm);
+		// 3、获得文件保存的路径；
+		String docmSavePath = "E:/Workspace/Spring Tool Suite/SpringMVCUploadAndDownload/WebContent/WEB-INF/upload";
+		// 4、构建文件file对象；
+		File fi = new File(docmSavePath, docm);
+		// 5、通过工具类实现文件上传。
+//		FileUtils.copyInputStreamToFile(file.getInputStream(), fi);
+
+		return new ModelAndView("index");
+	}
 }
